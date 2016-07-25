a6dcf0b24959c933bd596eda895ba108bc2f4ba8
playzig <clueplayzig@gmail.com>
2016-07-26 01:16:49 +0900

    study

--- app/src/main/java/com/example/cluep/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cluep/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.cluep.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Btn1 = (Button) findViewById(R.id.Btn1);
+        Btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"ddd",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
